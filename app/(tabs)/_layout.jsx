acf3594aa1af601b3fb773f938ac86053ff0c610
Tharushi-Nethmini <it22083296@my.sliit.lk>
2024-10-10 18:59:48 +0530

    Add some styles

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -26,76 +26,106 @@ const AppNavigator = () => {
             <Tab.Screen 
             name="Homepg1" 
             component={Homepg1} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="Homepg2" 
             component={Homepg2} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false ,
+                tabBarStyle: { display: 'none' }
+            }}
             />
             <Tab.Screen 
             name="Homepg3" 
             component={Homepg3} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="Homepg4" 
             component={Homepg4} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="TeacherHomePage" 
             component={TeacherHomePage} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false ,
+                tabBarStyle: { display: 'none' }
+            }}
             />
             <Tab.Screen 
             name="ParentHomePage" 
             component={ParentHomePage} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="TeacherHome" 
             component={TeacherHome}
-            options={{ headerShown: false }} 
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }} 
             />
             <Tab.Screen name="TeacherHomeWork" 
             component={TeacherHomeWork} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="DisplayTeacherHomework" 
             component={DisplayTeacherHomework} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="EditActivity" 
             component={EditActivity} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="Feedbacks" 
             component={Feedbacks} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="ParentHome" 
             component={ParentHome} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false ,
+                tabBarStyle: { display: 'none' }
+            }}
             />
             <Tab.Screen 
             name="DisplayParentHomework" 
             component={DisplayParentHomework} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="WorkSheet" 
             component={WorkSheet} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false,
+                tabBarStyle: { display: 'none' }
+             }}
             />
             <Tab.Screen 
             name="Ratings" 
             component={Ratings} 
-            options={{ headerShown: false }}
+            options={{ headerShown: false ,
+                tabBarStyle: { display: 'none' }
+            }}
             />
         </Tab.Navigator>
     );
